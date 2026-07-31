5a9022fe4831b1888b306518fefa8fddeb344a01
ashiyadav04 <ashiyadav959@gmail.com>
2026-07-31 11:23:49 +0530

    registration form

---
+++ index.js
@@ -0,0 +1,13 @@
+const name=document.getElementById("name");
+const emaile=document.getElementById("email");
+const password=document.getElementById("password");
+const btn=document.getElementById("btn");
+const handleClick=()=>{
+console.log(name);
+console.log(emil);
+console.log(password);
+console.log(btn);
+
+
+}
+btn.addEventListener("click",handleClick);
