0c3c36041add2fe03710542fe14cebfadebc5181
ashiyadav04 <ashiyadav959@gmail.com>
2026-07-31 12:03:00 +0530

    Datatypes

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+"use strict";//use all js code as newer version
+// alert("Ashi yadav") //we are using node js not browser 
+
+// console.log(3+3)  console.log("ashi") error and zero code readability
+//use new lines 
+console.log(3+3)
+console.log("Ashi")
+let name="Ashi" //string
+let age=22 //number
+let isloggedin=false//boolean
+
+//Datatypes
+
+//1.primitive datatypes
+// number=>range 2to power 53
+//bigint
+//string=""
+//boolean =>true/false
+//null-> standalone value  //a representation of empty value 
+//undefined=>when value not assigned only variable declared
+//let state=null
+//symbol is used to uniqueness
+
+//2.object
+console.log(typeof age)
+console.log(typeof null)
+//output object here null is a type of object 
+
+console.log(typeof undefined )
+//output undefined 
+
+
+
