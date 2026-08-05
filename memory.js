d85c413d5d434cc8051bfb9e879f4520b89043e7
ashiyadav04 <ashiyadav959@gmail.com>
2026-08-05 11:53:51 +0530

    stack and heap

---
+++ memory.js
@@ -0,0 +1,21 @@
+//Stack(primitive datatype)
+//heap(non primitive datatype)
+
+
+let myname="Ashiyadav";
+let anothername=myname;
+anothername="monika"
+console.log(anothername);
+console.log(myname);
+
+let userone={
+    email:"user@google.com",
+    upi:"user@yb l"
+}
+let usertwo=userone;
+usertwo.email="ashi@google.com"
+console.log(userone)
+console.log(usertwo)
+
+console.log(userone.email)
+console.log(usertwo.email)
