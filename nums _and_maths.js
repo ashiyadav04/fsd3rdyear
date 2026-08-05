11e47b5820327f7ebae8a88e2b713b72789b328c
ashiyadav04 <ashiyadav959@gmail.com>
2026-08-05 13:23:39 +0530

    Math and nums revision

---
+++ nums _and_maths.js
@@ -0,0 +1,46 @@
+const score=400
+console.log(score);
+
+
+
+const balance= new Number(100)
+console.log(balance);
+console.log(balance.toString().length)
+console.log(typeof balance.toString())
+
+
+console.log(balance.toFixed(2))
+
+const othernumber=23.8966
+
+console.log(othernumber)
+console.log( typeof othernumber)
+
+console.log(othernumber.toFixed(3))
+console.log(othernumber.toString())
+console.log(othernumber.toString().length)
+
+
+console.log(othernumber.toPrecision(3))
+
+const hundreds=1000000
+
+console.log(hundreds.toLocaleString())
+
+
+
+//++++++++++++++++  Maths +++++++++++++++++++++//
+
+
+console.log(Math)
+console.log(Math.round(4.4))
+console.log(Math.ceil(4.1))
+
+console.log(Math.floor(4))
+console.log(Math.min(7,4,5,6,1))
+console.log((Math.random()*10)+1)
+
+console.log(Math.floor(Math.random()*10)+1)
+
+
+
