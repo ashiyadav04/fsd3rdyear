688981ab68041e74a33cda4d81f553ac612f96ee
ashiyadav04 <ashiyadav959@gmail.com>
2026-08-14 14:32:45 +0530

    switch

---
+++ JavaScript/switch.js
@@ -0,0 +1,69 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month=3
+// switch (month) {
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("Feb")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+//     case 5:
+//         console.log("may")
+//         break;
+//     case 6:
+//         console.log("June")
+//         break;
+//     case 7:
+//         console.log("july")
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+//when we take string 
+
+const month="jan"
+switch (month) {
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("Feb")
+        break;
+    case "march":
+        console.log("march")
+        break;
+    case "april":
+        console.log("april")
+        break;
+    case "may":
+        console.log("may")
+        break;
+    case "june":
+        console.log("June")
+        break;
+    case "july":
+        console.log("july")
+        break;
+
+    default:
+        break;
+}
+
+
